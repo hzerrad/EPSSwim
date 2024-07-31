b1dbc4632ce829aba6071ad59411ef8e2a8ec8e0
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-07-31 09:42:32 +0100

    Added the blue splashscreen

--- app/src/main/java/com/example/epsswim/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.epsswim.presentation.ui.theme.EPSSwimTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             EPSSwimTheme {
