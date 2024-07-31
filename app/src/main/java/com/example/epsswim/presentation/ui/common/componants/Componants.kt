859ff8f36f6bcf154ac710ab0f4065b8fe220694
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-07-31 16:10:23 +0100

    Created the TopAppBar

--- app/src/main/java/com/example/epsswim/presentation/ui/common/componants/Componants.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/common/componants/Componants.kt
@@ -1,15 +1,27 @@
 package com.example.epsswim.presentation.ui.common.componants
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.epsswim.R
+import com.example.epsswim.presentation.ui.theme.MyBackground
+import com.example.epsswim.presentation.ui.theme.MyPrimaryDark
 
 @Composable
 fun MyOutlinedTextField(
@@ -40,4 +52,30 @@ fun MyOutlinedTextField(
             keyboardOptions = keyboardOptions
         )
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppBar(
+    title: String,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable() (RowScope.() -> Unit) = {},
+){
+    CenterAlignedTopAppBar(
+        title = { 
+            Text(
+                text = title,
+                fontFamily = FontFamily(listOf(Font(R.font.cairo_semi_bold))),
+                fontSize = 24.sp,
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
+        navigationIcon = navigationIcon,
+        actions = actions,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            titleContentColor = MyBackground,
+            actionIconContentColor = MyBackground,
+            navigationIconContentColor = MyBackground,
+            containerColor = MyPrimaryDark
+        )
+    )
 }
