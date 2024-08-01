33f10401341de22b191c8b89973f247845a43d58
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-08-01 17:38:21 +0100

    Finished profile screen

--- app/src/main/java/com/example/epsswim/presentation/ui/common/componants/Componants.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/common/componants/Componants.kt
@@ -1,32 +1,49 @@
 package com.example.epsswim.presentation.ui.common.componants
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
@@ -147,5 +164,113 @@ fun MyBottomBar() {
             }
         }
 
+    }
+}
+@Composable
+fun CompetitionCard(modifier: Modifier,name: String, date: String, onClick: () -> Unit) {
+    OutlinedCard(
+        onClick = { onClick() },
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.outlinedCardColors(
+            containerColor = Color.White
+        ),
+        border = BorderStroke(0.6.dp, Color.LightGray)
+    ){
+        Row (
+            modifier= Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = name,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp,
+                )
+                Text(
+                    text = date,
+                    color = Color.Gray,
+                    fontSize = 13.sp,
+                )
+            }
+            Image(
+                modifier = Modifier.height(50.dp),
+                painter = painterResource(id = R.drawable.competition_badge),
+                contentDescription = "competition badge",
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileCard(modifier: Modifier, title: String, icon: Int, content: @Composable () -> Unit) {
+    var isClicked by remember {
+        mutableStateOf(false)
+    }
+    ElevatedCard(
+        onClick = { isClicked = !isClicked },
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(16.dp)
+                    .align(Alignment.CenterStart),
+                painter = painterResource(id = if (isClicked) R.drawable.chevron_up else R.drawable.chevron_down),
+                contentDescription = "icon",
+                tint = MyPrimaryDark
+            )
+            Row(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = title,
+                    fontFamily = FontFamily(listOf(Font(R.font.cairo_bold))),
+                    color = MyPrimaryDark,
+                    fontSize = 20.sp,
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = icon),
+                    contentDescription = "icon",
+                    tint = MyPrimaryDark
+                )
+            }
+        }
+        if (isClicked)
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 1.dp)
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color.LightGray)
+
+            )
+        AnimatedVisibility(visible = isClicked) {
+            Column (
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp).fillMaxWidth(),
+                horizontalAlignment = Alignment.End
+            ) {
+                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl){
+                    content()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/epsswim/presentation/ui/common/screens/SwimmerProfile.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/common/screens/SwimmerProfile.kt
@@ -1,46 +1,30 @@
-package com.example.epsswim.presentation.ui.parent.screens
+package com.example.epsswim.presentation.ui.common.screens
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -50,13 +34,12 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.epsswim.R
+import com.example.epsswim.presentation.ui.common.componants.CompetitionCard
+import com.example.epsswim.presentation.ui.common.componants.ProfileCard
 import com.example.epsswim.presentation.ui.theme.MyBackground
-import com.example.epsswim.presentation.ui.theme.MyPrimary
-import com.example.epsswim.presentation.ui.theme.MyPrimaryDark
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,6 +49,7 @@ fun SwimmerProfile(){
         modifier = Modifier
             .background(MyBackground)
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ){
         Box {
 
@@ -146,6 +130,7 @@ fun SwimmerProfile(){
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
             )
             Text(
                 text = buildAnnotatedString {
@@ -165,6 +150,8 @@ fun SwimmerProfile(){
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
+
             )
             Text(
                 text = buildAnnotatedString {
@@ -184,6 +171,8 @@ fun SwimmerProfile(){
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
+
             )
             Text(
                 text = buildAnnotatedString {
@@ -203,6 +192,8 @@ fun SwimmerProfile(){
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
+
             )
             Text(
                 text = buildAnnotatedString {
@@ -222,6 +213,7 @@ fun SwimmerProfile(){
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
             )
             Text(
                 text = buildAnnotatedString {
@@ -238,92 +230,52 @@ fun SwimmerProfile(){
                     ) {
                         append("055454545454")
                     }
-                    Text(
-                        text = buildAnnotatedString {
-                            withStyle(style = SpanStyle(
-                                fontWeight = FontWeight.Bold
-                            )
-                            ) {
+                },
+                color = Color.Black,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 14.dp)
 
-                                append(stringResource(R.string.abscence_number))
-                            }
-                            withStyle(style = SpanStyle(
-                                fontWeight = FontWeight.Bold
-                            )
-                            ) {
-                                append('3')
-                            }
-                        },
-                        color = Color.Black,
-                        fontSize = 16.sp,
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(
+                        fontWeight = FontWeight.Bold
                     )
+                    ) {
+
+                        append(stringResource(R.string.abscence_number))
+                    }
+                    withStyle(style = SpanStyle(
+                        fontWeight = FontWeight.Bold
+                    )
+                    ) {
+                        append('3')
+                    }
                 },
                 color = Color.Black,
                 fontSize = 16.sp,
             )
         }
+        ProfileCard(
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 30.dp),
+            title = stringResource(R.string.competition),
+            icon = R.drawable.competition_profile_ic
+        ){
+            CompetitionCard(
+                modifier=Modifier.padding(bottom = 16.dp),
+                name="المسابقة الولائية",
+                date = "12/08/2023"
+            ){
+
+            }
+            CompetitionCard(
+                modifier=Modifier,
+                name="المسابقة الولائية",
+                date = "12/08/2023"
+            ){
+
+            }
+        }
     }
 }
 
-@Composable
-fun ProfileCard(modifier: Modifier, title: String, icon: Int, content: @Composable () -> Unit) {
-    var isClicked by remember {
-        mutableStateOf(false)
-    }
-    ElevatedCard(
-        onClick = { isClicked = !isClicked },
-        modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.elevatedCardColors(
-            containerColor = Color.White
-        ),
-        elevation = CardDefaults.cardElevation(5.dp)
-    ){
-        Box(
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 24.dp)
-                .fillMaxWidth()
-        ) {
-            Icon(
-                modifier = Modifier
-                    .size(16.dp)
-                    .align(Alignment.CenterStart),
-                painter = painterResource(id = if (isClicked) R.drawable.chevron_up else R.drawable.chevron_down),
-                contentDescription = "icon",
-                tint = MyPrimaryDark
-            )
-            Row(
-                modifier = Modifier.align(Alignment.CenterEnd),
-                verticalAlignment = Alignment.CenterVertically
-            ){
-                Text(
-                    text = title,
-                    fontFamily = FontFamily(listOf(Font(R.font.cairo_bold))),
-                    color = MyPrimaryDark,
-                    fontSize = 20.sp,
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Icon(
-                    modifier = Modifier.size(24.dp),
-                    painter = painterResource(id = icon),
-                    contentDescription = "icon",
-                    tint = MyPrimaryDark
-                )
-            }
-        }
-        if (isClicked)
-            Box(
-                modifier = Modifier
-                    .size(1.dp)
-                    .background(MyBackground)
-                    .fillMaxWidth()
-            )
-        AnimatedVisibility(visible = isClicked) {
-            Column (modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp)) {
-                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl){
-                    content()
-                }
-            }
-        }
-    }
-}
