94a2d9e1813e0d1168d07a5ed423b4e6c66be10e
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-08-03 16:28:31 +0100

    Moving forward with the calenar design

--- app/src/main/java/com/example/epsswim/presentation/ui/trainer/componants/Componants.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/trainer/componants/Componants.kt
@@ -1,28 +1,44 @@
 package com.example.epsswim.presentation.ui.trainer.componants
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.epsswim.R
+import com.example.epsswim.presentation.ui.theme.MyBackground
+import com.example.epsswim.presentation.ui.theme.MyPrimary
+import com.example.epsswim.presentation.ui.theme.MyPrimaryDark
 import com.example.epsswim.presentation.ui.theme.MySecondary
-import com.kizitonwose.calendar.compose.HorizontalCalendar
-import com.kizitonwose.calendar.compose.VerticalCalendar
 import com.kizitonwose.calendar.compose.WeekCalendar
 import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
-import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.WeekDay
 import com.kizitonwose.calendar.core.atStartOfMonth
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
@@ -54,33 +70,84 @@ fun LevelCard(modifier: Modifier, title: String, onClick: () -> Unit) {
 @Preview
 @Composable
 fun MyWeekCalendar(){
-    val currentDate = remember { LocalDate.now() }
-    val currentMonth = remember { YearMonth.now() }
-    val startDate = remember { currentMonth.minusMonths(100).atStartOfMonth() } // Adjust as needed
-    val endDate = remember { currentMonth.plusMonths(100).atEndOfMonth() } // Adjust as needed
-    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        val currentDate = remember { LocalDate.now() }
+        val currentMonth = remember { YearMonth.now() }
+        val startDate = remember { currentMonth.atStartOfMonth() } // Adjust as needed
+        val endDate = remember { currentMonth.plusMonths(1).atEndOfMonth() } // Adjust as needed
+        val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
 
-    val state = rememberWeekCalendarState(
-        startDate = startDate,
-        endDate = endDate,
-        firstVisibleWeekDate = currentDate,
-        firstDayOfWeek = firstDayOfWeek
-    )
 
-    WeekCalendar(
-        state = state,
-        calendarScrollPaged = false,
-        dayContent = { Day(it) }
-    )
+        val state = rememberWeekCalendarState(
+            startDate = startDate,
+            endDate = endDate,
+            firstVisibleWeekDate = currentDate,
+            firstDayOfWeek = firstDayOfWeek
+        )
+        val configuration = LocalConfiguration.current
+        val screenWidth = configuration.screenWidthDp.dp *0.95f
+        val itemDp = screenWidth/7
+
+       Surface (color = MyPrimaryDark) {
+           Column (
+               modifier = Modifier.padding(10.dp)
+           ){
+               Text(
+                   modifier = Modifier.padding(bottom = 25.dp),
+                   text = currentDate.toString(),
+                   fontFamily = FontFamily(listOf(Font(R.font.cairo_bold))),
+                   fontSize = 18.sp,
+                   color = MyBackground
+               )
+               WeekCalendar(
+                   state = state,
+                   calendarScrollPaged = false,
+                   dayContent = {
+                       Day(
+                           day = it,
+                           selected = it.date == currentDate,
+                           dp = itemDp
+                       )
+                   }
+               )
+           }
+       }
+    }
+
 
 }
 @Composable
-fun Day(day: WeekDay) {
+fun Day(day: WeekDay, selected: Boolean = false, dp: Dp) {
     Box(
-        modifier = Modifier
-            .aspectRatio(1f), // This is important for square sizing!
+        modifier =
+        Modifier
+            .width(dp)
+            .padding(end = 5.dp)
+            .background(if (!selected) MyPrimaryDark else MyBackground, RoundedCornerShape(12.dp)),
+//            .aspectRatio(1f), // This is important for square sizing!
         contentAlignment = Alignment.Center
     ) {
-        Text(text = day.date.dayOfMonth.toString())
+        Column (
+            Modifier.padding(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val colorText = if (!selected) MyBackground else MyPrimaryDark
+            Text(
+                text = day.date.dayOfWeek.name.removeRange(3,day.date.dayOfWeek.name.length),
+                color = colorText,
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Clip
+            )
+            Text(
+                modifier = Modifier.padding(top = 16.dp),
+                text = day.date.dayOfMonth.toString(),
+                color = colorText,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+            )
+//            Text(text = day.date.dayOfWeek.name)
+        }
     }
 }
