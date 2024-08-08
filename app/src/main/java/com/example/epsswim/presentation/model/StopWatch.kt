f95085d92164a3d40187ad38fb9b2ccab4653a98
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-08-08 11:12:00 +0100

    Added Stop Watch Class

---
+++ app/src/main/java/com/example/epsswim/presentation/model/StopWatch.kt
@@ -0,0 +1,65 @@
+package com.example.epsswim.presentation.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class StopWatch {
+    var formattedTime by mutableStateOf("00:00:000")
+
+    private var coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var isActive = false
+
+    private var timeMillis = 0L
+    private var lastTimestamp = 0L
+
+    fun start() {
+        if(isActive) return
+
+        coroutineScope.launch {
+            lastTimestamp = System.currentTimeMillis()
+            this@StopWatch.isActive = true
+            while(this@StopWatch.isActive) {
+                delay(10L)
+                timeMillis += System.currentTimeMillis() - lastTimestamp
+                lastTimestamp = System.currentTimeMillis()
+                formattedTime = formatTime(timeMillis)
+            }
+        }
+    }
+
+    fun pause() {
+        isActive = false
+    }
+
+    fun reset() {
+        coroutineScope.cancel()
+        coroutineScope = CoroutineScope(Dispatchers.Main)
+        timeMillis = 0L
+        lastTimestamp = 0L
+        formattedTime = "00:00:000"
+        isActive = false
+    }
+
+    private fun formatTime(timeMillis: Long): String {
+        val localDateTime = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(timeMillis),
+            ZoneId.systemDefault()
+        )
+        val formatter = DateTimeFormatter.ofPattern(
+            "mm:ss:SSS",
+            Locale.getDefault()
+        )
+        return localDateTime.format(formatter)
+    }
+}
