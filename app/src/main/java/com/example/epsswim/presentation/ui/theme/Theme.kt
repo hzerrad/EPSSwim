000c03998ecf48ffce44f50ff19a853ad1de1492
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-09-03 13:45:00 +0100

    only light theme

--- app/src/main/java/com/example/epsswim/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/epsswim/presentation/ui/theme/Theme.kt
@@ -44,7 +44,7 @@ fun EPSSwimTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicLightColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> LightColorScheme
