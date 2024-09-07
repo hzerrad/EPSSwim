88ab4db7a8b83645b1f6e3c4c760a84fb2af6dde
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-09-07 15:03:44 +0100

    version 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.epsswim"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
